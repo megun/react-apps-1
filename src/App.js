5e2537148f5164465e6659bf4a2f45cd74ac44f8
megun <akamarl.1127@gmail.com>
2021-10-27 13:22:24 +0900

    change class

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
-  );
+//function App() {
+//  return (
+//    <div>
+//      <h1>Hello, World!</h1>
+//    </div>
+//  );
+
+class App extends Component {
+  render() {
+    return <h1>Hello, world!</h1>;
+  }
 }
 
 export default App;
